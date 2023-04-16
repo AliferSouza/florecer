f40da35a23896c8e8d2b9ceb41d7d2414a0897e7
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2023-04-16 00:10:39 -0300

    Add files via upload

--- src/dados/data.js
+++ src/dados/data.js
@@ -2,7 +2,7 @@
 const cardDateColaboradores = [    
     {id:"vivi",img :"./src/img/vivi.jpg",vivi:'https://www.instagram.com/viviane.snt_/'},
     {id:"alifer", img:"./src/img/alifer.jpg",alifer:'https://www.instagram.com/alifer_oliveira_souza/'},
-    {id:"ester",img:"./src/img/Ester.jpg",ester:'https://www.instagram.com/grazileite.integrar/'},  
+    {id:"vivi",img :"./src/img/vivi.jpg",vivi:'https://www.instagram.com/viviane.snt_/'},  
     {id:"lest",img:"./src/img/lest.jpg",lest:'https://www.instagram.com/_drac_lb/'}
   
  
