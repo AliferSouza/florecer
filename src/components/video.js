94c821c0a41b1d70cf161591ab99fc7225933e54
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2023-04-30 22:24:38 -0300

    Update video.js

--- src/components/video.js
+++ src/components/video.js
@@ -3,7 +3,7 @@ export default function video() {
   return `
 
 <video id="videoPalyPause"  width="200" height="300" controls preload="auto">
-<source src="./src/img/vivi.mp4" type="video/mp4">
+<source src="https://drive.google.com/file/d/1IaEKZU0CwgSvP8Z1dKKL1Qt6GZN0psUF/preview" type="video/mp4">
 </video>
 <button id="fechar-pop" onclick="aparecerFecha()">Fechar</button>
 
