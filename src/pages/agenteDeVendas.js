2a4cb99be309994e933d928acc0630fe4e33bd65
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2023-05-24 18:46:21 -0300

    Update agenteDeVendas.js

--- src/pages/agenteDeVendas.js
+++ src/pages/agenteDeVendas.js
@@ -3,7 +3,7 @@ export default async function vendas() {
     return `
         <div class="container_vendas">
 		<img src="./src/img/vendedor.jpeg"  width="40" height="40">	
-		<a href="https://wa.me/5531971253590?text=Oi%2C+tudo+bem%21+Tenho+interesse+no+Workshop." target="_blank">ENTRE EM CONTATO</a>
+		<a href="https://wa.me/5531971253590?text=Tenho+interesse+na+candidatura+home+office." target="_blank">ENTRE EM CONTATO</a>
        </div>	
  
  
